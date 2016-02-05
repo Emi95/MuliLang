55701c417783b146abbf537d3a677e6b62b570ae
Elmira_Aueskhan <the_secret_princess@mail.ru>
2016-02-05 16:36:59 +0600

    Changed project

--- app/src/main/java/com/example/demo/mulilang/MainActivity.java
+++ app/src/main/java/com/example/demo/mulilang/MainActivity.java
@@ -2,8 +2,6 @@ package com.example.demo.mulilang;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
 
 public class MainActivity extends Activity {
@@ -12,7 +10,7 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+//sdjnkfnsnfnkldnfnslkdn
 
 
     }
